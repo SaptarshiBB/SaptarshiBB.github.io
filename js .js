56f1e6b20fc0a0551b207bf5bbbed0d02d13e33f
SaptarshiBB <saptarshineymar88@gmail.com>
2023-11-27 23:20:44 +0530

    first commit

---
+++ js .js
@@ -0,0 +1,8 @@
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 100) {
+    header.style.backgroundColor = "#FFD6FF";
+    header.style.backgroundColor = "#FFD6FF";
+  }
+});
